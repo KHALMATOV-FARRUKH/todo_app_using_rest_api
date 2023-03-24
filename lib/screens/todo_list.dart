2eb3755a1f82f5b1913ec34d1a7791710025d317
KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>
2023-03-24 22:19:53 +0500

    Added dependence: http: ^0.13.5
    Building User Interface
    Create Todo Task[POST]
    List All Todo[GET]
    Create Todo Task[Refresh Indicator]

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:todo_app_using_rest_api/screens/add_page.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({Key? key}) : super(key: key);
@@ -9,21 +12,66 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  bool isLoading = true;
+  List items = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Todo App"),
+        title: Text("Todo List"),
+      ),
+      body: Visibility(
+        visible: isLoading,
+        child: Center(child: CircularProgressIndicator()),
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (context, index) {
+                final item = items[index] as Map;
+                return ListTile(
+                  leading: CircleAvatar(child: Text("${index + 1}")),
+                  title: Text(item['title']),
+                  subtitle: Text(item['description']),
+                );
+              }),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
           onPressed: () {
             navigateToAddPage();
-          }, label: Text("Add Todo")),
+          },
+          label: Text("Add Todo")),
     );
   }
 
   void navigateToAddPage() {
     final route = MaterialPageRoute(builder: (context) => AddTodoPage());
     Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    }
+
+    setState(() {
+      isLoading = false;
+    });
   }
 }
