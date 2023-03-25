f16a1460f013f9848bdd8de395480fc2cf6c3e02
KHALMATOV-FARRUKH <khalmatov.farrukh@gmail.com>
2023-03-25 13:31:50 +0500

    Create Todo Task [DELETE]

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -36,10 +36,35 @@ class _TodoListPageState extends State<TodoListPage> {
               itemCount: items.length,
               itemBuilder: (context, index) {
                 final item = items[index] as Map;
+
+                final id = item['_id'] as String;
+
                 return ListTile(
                   leading: CircleAvatar(child: Text("${index + 1}")),
                   title: Text(item['title']),
                   subtitle: Text(item['description']),
+                  trailing: PopupMenuButton(
+                    onSelected: (value) {
+                      if (value == "edit") {
+                        // open Edit page
+                      } else if (value == "delete") {
+                        // delete or remove
+                        deleteById(id);
+                      }
+                    },
+                    itemBuilder: (context) {
+                      return [
+                        const PopupMenuItem(
+                          child: Text("Edit"),
+                          value: "edit",
+                        ),
+                        const PopupMenuItem(
+                          child: Text("Delete"),
+                          value: "delete",
+                        ),
+                      ];
+                    },
+                  ),
                 );
               }),
         ),
@@ -55,6 +80,21 @@ class _TodoListPageState extends State<TodoListPage> {
   void navigateToAddPage() {
     final route = MaterialPageRoute(builder: (context) => AddTodoPage());
     Navigator.push(context, route);
+  }
+
+  Future<void> deleteById(String id) async {
+    final url = 'https://api.nstack.in/v1/todos/$id';
+    final uri = Uri.parse(url);
+    final response = await http.delete(uri);
+
+    if (response.statusCode == 200) {
+      final filtered = items.where((element) => element['_id'] != id).toList();
+      setState(() {
+        items = filtered;
+      });
+    } else {
+      showErrorMessage('Deletion Failed');
+    }
   }
 
   Future<void> fetchTodo() async {
@@ -73,5 +113,19 @@ class _TodoListPageState extends State<TodoListPage> {
     setState(() {
       isLoading = false;
     });
+  }
+
+
+
+
+  void showErrorMessage(String message) {
+    final snackBar = SnackBar(
+      content: Text(
+        message,
+        style: TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.red,
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
   }
 }
